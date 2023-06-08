f79031bf83b34991d4024f6dbfd00110caaaf8f7
Franco Pecci <fapeccisan@gmail.com>
2023-06-08 10:04:48 -0400

    a

--- Prueba Técnica/src/components/card.jsx
+++ Prueba Técnica/src/components/card.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
+import "./card.css"
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function Card() {
+
+  const [info, setInfo] = useState([])
+
+  const llamar = () => {
+    axios.get("https://rickandmortyapi.com/api/character")
+    .then(result => {
+      setInfo(result.data.results)
+    })
+  }
+
+  useEffect(() => {
+    llamar()
+  })
+
   return (
-    <div className='container'>
-      <div className='row mb-3'>
-        <div className='col-md-4'>
-          <div className="card" style={{ width: '15rem' }}>
-            <img src="images/guns.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div className='col-md-4'>
-          <div className="card" style={{ width: '15rem' }}>
-            <img src="images/guns.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
+    <div>
+      <div>
+        <h2>Rick and Morty characters</h2>
       </div>
+      <h2></h2>
+        <div className='container d-flex  justify-content-center align-items-center flex-wrap'>
+          {info.map((informacion, index) => (
+          <div key={index} className='row m-2'>
+            <div className='col-md-3'>
+              <div className="card" style={{ width: '15rem' }}>
+                <img src={informacion.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{informacion.name}</h5>
+                  <p className="card-text">{informacion.status}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          ))}
+        </div>
     </div>
   );
 }
