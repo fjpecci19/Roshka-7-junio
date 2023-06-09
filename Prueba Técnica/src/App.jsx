dc4df33e4ee1f28720d48bf2513ae37c5fbb0020
Franco Pecci <fapeccisan@gmail.com>
2023-06-09 09:40:36 -0400

    a

--- Prueba Técnica/src/App.jsx
+++ Prueba Técnica/src/App.jsx
@@ -1,12 +1,13 @@
 import Card from "./components/card"
+import Pagination from "./components/pagination"
 
 function App() {
   return (
     <div>
       <Card/>
+      <Pagination/>
     </div>
   )
 }
 
 export default App
- 
---
+++ Prueba Técnica/src/components/pagination.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Pagination() {
+  return (
+    <div>
+      <ul class="pagination d-flex justify-content-center">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">4</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Pagination
